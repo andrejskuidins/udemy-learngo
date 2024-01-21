ce36162604b40eae1a0aeae7d6aeba53e5910337
Andrejs Kuidins <andrejs.kuidins@1nce.com>
2024-01-21 18:16:35 +0200

    master

---
+++ __custom/banger.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "strings"
+)
+
+func addExclam(s string) string {
+  var exclam string
+  for range s {
+    exclam += "!"
+  }
+  return exclam
+}
+
+func main() {
+  if len(os.Args) < 2 {
+    fmt.Println("Please provide a command-line argument.")
+    return
+  }
+
+  argWithProg := os.Args[1]
+  argWithProgUpper := strings.ToUpper(argWithProg)
+
+  exclamString := addExclam(argWithProg)
+  fmt.Println(argWithProgUpper + exclamString)
+}
