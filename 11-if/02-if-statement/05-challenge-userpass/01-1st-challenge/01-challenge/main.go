69b0d88e259206f5a4a114f187288a2ca43da879
Andrejs Kuidins <andrejs.kuidins@1nce.com>
2024-01-22 22:33:11 +0200

    master

--- 11-if/02-if-statement/05-challenge-userpass/01-1st-challenge/01-challenge/main.go
+++ 11-if/02-if-statement/05-challenge-userpass/01-1st-challenge/01-challenge/main.go
@@ -6,8 +6,6 @@
 // In-person training  : https://www.linkedin.com/in/inancgumus/
 // Follow me on twitter: https://twitter.com/inancgumus
 
-package main
-
 // ---------------------------------------------------------
 // CHALLENGE #1
 //  Create a user/password protected program.
@@ -33,5 +31,24 @@ package main
 //    Access granted to "jack".
 // ---------------------------------------------------------
 
+package main
+import (
+  "fmt"
+	"os"
+)
+
 func main() {
+	username, password := "jack", "1888"
+
+	l := os.Args
+
+	if len(l) < 3 {
+    fmt.Println("Usage: [username] [password]")
+  }	else if l[1] != username {
+		fmt.Printf("Access denied for %q.\n", l[1])
+	} else if l[2] != password {
+		fmt.Printf("Invalid password for %q.\n", l[1])
+	} else {
+		fmt.Printf("Access granted to %q.\n", l[1])
+	}
 }
