d48996f3076840b4e61c3ed583548abaef1d2591
Andrejs Kuidins <andrejs.kuidins@1nce.com>
2024-02-03 22:59:12 +0200

    master

--- 15-project-retro-led-clock/main.go
+++ 15-project-retro-led-clock/main.go
@@ -1,34 +1,17 @@
 package main
 import (
     "fmt"
-    "math/rand"
     "time"
-		"os"
-		"strconv"
-)
-
-const (
-	usage = "[your name] [positive|negative]"
-	mood = "%s feels %s\n"
 )
 
 func main()  {
-	if len(os.Args) != 2 {
-		fmt.Println("Give me millis")
-		return
+	digits := [5][10]string{
+		{"###", "## ", "## ", "## ", "## ", "## ", "## ", "## ", "## ", "## "},
+		{"# #", " # ", "## ", "## ", "## ", "## ", "## ", "## ", "## ", "## "},
+		{"# #", " # ", "## ", "## ", "## ", "## ", "## ", "## ", "## ", "## "},
+		{"# #", " # ", "## ", "## ", "## ", "## ", "## ", "## ", "## ", "## "},
+		{"###", "###", "## ", "## ", "## ", "## ", "## ", "## ", "## ", "## "},
 	}
 
-	d, err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		fmt.Println("No millis provided. x10")
-		return
-	}
-
-	millis := time.Duration(d)
-	s := "\\/|-"
-
-	for {
-		fmt.Printf("\r%s Please Wait. Processing....\n", string(s[rand.Intn(4)]))
-		time.Sleep(millis * time.Millisecond)
-	}
+	fmt.Printf("\r%-v \n", digits)
 }
