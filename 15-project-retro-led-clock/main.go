0622973e6036db0279301bc06b8596d8541af8d5
Andrejs Kuidins <andrejs.kuidins@1nce.com>
2024-02-04 14:47:46 +0200

    master

--- 15-project-retro-led-clock/main.go
+++ 15-project-retro-led-clock/main.go
@@ -15,9 +15,7 @@ func main()  {
 
 	for {
 		fmt.Print("\033[H\033[2J")
-		t := time.Now()
-		// formatted := fmt.Sprintf("%02d:%02d:%02d", t.Hour(), t.Minute(), t.Second())
-		// fmt.Println(formatted)
+		t := time.Now()tf("%02d:%02d:%02d", t.Hour(), t.Minute(), t.Second())
 
 		hourStr := fmt.Sprintf("%02d", t.Hour())
 		f1 := int(hourStr[0] - '0')
@@ -31,7 +29,6 @@ func main()  {
 		f5 := int(secondStr[0] - '0')
 		f6 := int(secondStr[1] - '0')
 
-		// fmt.Println(f1, f2, f3, f4, f5, f6)
 		for i := 0; i < len(digits); i++ {
 			if f6 % 2 == 0 {
 				fmt.Println(digits[i][f1], digits[i][f2], digits[i][10], digits[i][f3], digits[i][f4], digits[i][10], digits[i][f5], digits[i][f6])
@@ -42,5 +39,4 @@ func main()  {
 		time.Sleep(1000 * time.Millisecond)
 		fmt.Print("\033[H\033[2J")
 	}
-
 }
