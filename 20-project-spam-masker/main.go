0a76d68198e8aaa57e44f73498102ae0d9892131
Andrejs Kuidins <andrejs.kuidins@1nce.com>
2024-03-10 22:05:01 +0200

    master

--- 20-project-spam-masker/main.go
+++ 20-project-spam-masker/main.go
@@ -8,14 +8,27 @@ import (
 
 func main()  {
 	link := os.Args[1]
-	for _, v := range strings.Split(link, " ")  {
-		if v[0:6] == "http://" {
-			buf := v[0:6]
-			for _, r := range buf[6:] {
-				buf = append(buf, "*")
+	sent := strings.Split(link, " ")
+	buf := make([]string, 0)
+	final := make([]string, 0)
+	var place int
+
+
+	for i, v := range sent {
+		if len(v) > 6 {
+			fmt.Printf(v[0:6])
+			if v[0:6] == "http://" {
+				buf := append(buf, v[0:6])
+				for i := 0; i < len(v[6:]); i++ {
+					buf = append(buf, "*")
+				}
+				place = i
 			}
 		}
 
-		fmt.Printf("%q\n", v)
+		final = append(sent[:0], sent[0:place]...)
+		final = append(final, buf...)
+		final = append(final, sent[place+1:]...)
 	}
+	fmt.Printf("%q\n", final)
 }
