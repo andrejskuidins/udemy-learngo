593de179fd0d308ded2746b809940f40687f3a01
Andrejs Kuidins <andrejs.kuidins@1nce.com>
2024-03-13 21:34:26 +0200

    master

--- 20-project-spam-masker/main.go
+++ 20-project-spam-masker/main.go
@@ -9,19 +9,21 @@ import (
 func main() {
     link := os.Args[1]
     buf := make([]byte, 0)
+    splitted := strings.Split(link, " ")
 
-    for _, v := range strings.Split(link, " ") {
+    var parts []string
 
+    for _, v := range splitted {
         if len(v) > 7 && v[:7] == "http://" {
-            fmt.Println(v)
             for range v[7:] {
                 buf = append(buf, '*')
             }
+            modifiedURL := "http://" + string(buf)
+            parts = append(parts, modifiedURL)
+        } else {
+            parts = append(parts, v)
         }
     }
 
-		modifiedURL := "http://" + string(buf)[:]
-		fmt.Println(modifiedURL)
+    fmt.Println(strings.Join(parts, " "))
 }
-
-// go run main.go "Here: http://www.mylink.com Click!"
