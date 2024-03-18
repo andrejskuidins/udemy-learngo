4a7cec7ca8592e0278e91283be1afcd87b441fe7
Andrejs Kuidins <andrejs.kuidins@1nce.com>
2024-03-18 18:57:05 +0200

    master

--- 23-input-scanning/exercises/05-quit/main.go
+++ 23-input-scanning/exercises/05-quit/main.go
@@ -6,7 +6,6 @@
 // In-person training  : https://www.linkedin.com/in/inancgumus/
 // Follow me on twitter: https://twitter.com/inancgumus
 
-package main
 
 // ---------------------------------------------------------
 // EXERCISE: Quit
@@ -36,5 +35,50 @@ package main
 //   TWICE!
 // ---------------------------------------------------------
 
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	// "regexp"
+)
+
 func main() {
+	// rx := regexp.MustCompile(os.Args[1])
+	unique := make(map[string]bool)
+	grep := os.Args[1]
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		line := strings.ToLower(scanner.Text())
+		if strings.Contains(line, grep) {
+			fmt.Println(line)
+		}
+	}
+}
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	words := 0
+	unique := make(map[string]bool)
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+
+	for scanner.Scan() {
+		words++
+		unique[strings.ToLower(scanner.Text())] = true
+	}
+
+	// fmt.Println(unique)
+	fmt.Printf("There are %d words, %d of them are unique.\n", words, len(unique)) // Println will add back the final '\n'
 }
