859e7100b112528e90e480a5440484d178af467f
Andrejs Kuidins <andrejs.kuidins@1nce.com>
2024-04-01 19:34:26 +0300

    master

--- 24-structs/exercises/04-encode/main.go
+++ 24-structs/exercises/04-encode/main.go
@@ -6,7 +6,6 @@
 // In-person training  : https://www.linkedin.com/in/inancgumus/
 // Follow me on twitter: https://twitter.com/inancgumus
 
-package main
 
 // ---------------------------------------------------------
 // EXERCISE: Encode
@@ -60,7 +59,83 @@ package main
 //
 // ---------------------------------------------------------
 
-func main() {
-	// use your solution from the previous exercise
+package main
 
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strings"
+	"strconv"
+	"regexp"
+)
+
+type item struct {
+	id int
+	name string
+	price int
+}
+
+type game struct {
+	item item
+	genre string
+}
+
+func main() {
+	games := []game{
+		{
+			item:  item{1, "god of war", 59},
+			genre: "Action",
+		},
+		{
+			item:  item{2, "x-com 2", 49},
+			genre: "Adventure",
+		},
+		{
+			item:  item{3, "minecraft", 69},
+			genre: "RPG",
+		},
+	}
+
+	fmt.Printf("Andy's game store has %d games.\n", len(games))
+	// fmt.Printf("id  name          genre        price\n")
+
+	// index the games by id
+	byID := make(map[int]game)
+	for _, g := range games {
+		byID[g.item.id] = g
+	}
+
+	for {
+		scanner := bufio.NewScanner(os.Stdin)
+		fmt.Print("\n> l    : lists all the games")
+		fmt.Print("\n> id N : queries a game by id")
+		fmt.Print("\n> q    : quits\n\n")
+		// Scans a line from Stdin(Console)
+		scanner.Scan()
+		// Holds the string that scanned
+		text := strings.ToLower(scanner.Text())
+		switch text {
+		case "l":
+			for _, g := range games {
+				fmt.Printf("#%-2d %-13s %-12s $%-8d\n", g.item.id, g.item.name, g.genre, g.item.price)
+				continue
+			}
+		case "q":
+			fmt.Println("bye!")
+			return
+		}
+		pattern := `^id\s+(\d+)$`
+		re := regexp.MustCompile(pattern)
+		matches := re.MatchString(text)
+		if matches {
+			ind, _ := strconv.Atoi(strings.Split(text, " ")[1])
+			if ind > len(games) {
+				fmt.Println("sorry. I don't have the game")
+			} else {
+				g := byID[ind]
+				fmt.Printf("#%-2d %-13s %-12s $%-8d\n", g.item.id, g.item.name, g.genre, g.item.price,)
+			}
+		}
+	}
 }
